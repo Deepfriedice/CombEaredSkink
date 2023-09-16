ee30ba71f59fd7bd802d74283cccf205d29abbc6
Richy McGregor <Deepfriedice@gmail.com>
2023-09-16 21:04:06 +1000

    play random moves

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,24 @@
 ﻿using ChessChallenge.API;
+using System.Collections.Generic;
+using System;
 
 public class MyBot : IChessBot
 {
+    private readonly Random rng;
+
+    public MyBot()
+    {
+        rng = new();
+    }
+
+    private Move RandomMove(IList<Move> moves)
+    {
+        return moves[rng.Next(moves.Count)];
+    }
+
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+        return RandomMove(moves);
     }
 }
