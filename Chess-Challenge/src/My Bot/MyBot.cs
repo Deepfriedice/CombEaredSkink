e46768cbdd8ea7dc4cc27bf56a2312750dcd6537
Richy McGregor <Deepfriedice@gmail.com>
2023-09-16 21:14:33 +1000

    min opposing moves

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -11,6 +11,15 @@ public class MyBot : IChessBot
         rng = new();
     }
 
+    // Try to evaluate how good a position is.
+    // Higher number -> better for the current player.
+    private int BoardScore(Board board)
+    {
+        int moveCount = board.GetLegalMoves().Length;
+        return moveCount;
+    }
+
+    // Select a random move from a list.
     private Move RandomMove(IList<Move> moves)
     {
         return moves[rng.Next(moves.Count)];
@@ -18,7 +27,34 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
-        Move[] moves = board.GetLegalMoves();
-        return RandomMove(moves);
+        int bestScore = 1000;
+        List<Move> bestMoves = new List<Move>();
+        foreach (Move move in board.GetLegalMoves())
+        {
+            board.MakeMove(move);
+
+            // play a checkmate move immediately
+            if (board.IsInCheckmate())
+            {
+                return move;
+            }
+
+            // update the list of best moves
+            int score = BoardScore(board);
+            if (score == bestScore)
+            {
+                bestMoves.Add(move);
+            }
+            else if (score < bestScore)
+            {
+                bestScore = score;
+                bestMoves.Clear();
+                bestMoves.Add(move);
+            }
+            board.UndoMove(move);
+        }
+
+        // play a random move from the best moves
+        return RandomMove(bestMoves);
     }
 }
