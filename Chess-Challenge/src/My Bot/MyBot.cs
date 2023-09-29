41c6578d6e58f18d8c4ee6a07b6e8f6d17ca5764
Richy McGregor <Deepfriedice@gmail.com>
2023-09-30 04:01:01 +1000

    eval cache attempt

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,4 +1,4 @@
-using ChessChallenge.API;
+﻿using ChessChallenge.API;
 using System;
 using System.Collections.Generic;
 
@@ -11,6 +11,24 @@ public class MyBot : IChessBot
     private const double InitialSearchScale = 1.8;  // coefficient for initial searchDepth value
     private int searchDepth = 0;
     internal int evalCount = 0;  //#DEBUG
+    internal int cacheHits = 0;  //#DEBUG
+    internal int cacheMisses = 0;  //#DEBUG
+    internal int cacheEvictions = 0;  //#DEBUG
+
+    private class CacheItem
+    {
+        public CacheItem? prev;
+        public CacheItem? next;
+        public ulong key;
+        public int score;
+    }
+
+    private Dictionary<ulong, CacheItem> evalCache = new Dictionary<ulong, CacheItem>();
+    private CacheItem? evalCacheHead;  // head is first
+    private CacheItem? evalCacheTail;
+    private const int EvalCacheMaxLength = 1<<24;
+
+
 
     // How long do we have to think about our move?
     private int ThinkingTimeGoal(Timer timer)
@@ -35,6 +53,58 @@ public class MyBot : IChessBot
         // note that board.IsInCheck is forced to false
     }
 
+    private int CachedBoardScore(Board board)
+    {
+        ulong key = board.ZobristKey;
+        CacheItem? cacheItem;
+
+        if (evalCache.TryGetValue(key, out cacheItem))
+        {
+            cacheHits++;  //#DEBUG
+            // linked list bullshit - move cacheItem to the front of the list
+            if (evalCacheHead != cacheItem)
+            {
+                if (evalCacheTail == cacheItem)
+                    evalCacheTail = cacheItem.prev;
+                else
+                    cacheItem.next.prev = cacheItem.prev;
+                cacheItem.prev.next = cacheItem.next;
+                cacheItem.prev = null;
+                cacheItem.next = evalCacheHead;
+                if (evalCacheHead != null)
+                    evalCacheHead.prev = cacheItem;
+                evalCacheHead = cacheItem;
+            }
+        }
+        else
+        {
+            cacheMisses++;  //#DEBUG
+            cacheItem = new CacheItem();
+            cacheItem.score = BoardScore(board);
+            cacheItem.key = key;
+            evalCache[key] = cacheItem;
+            cacheItem.prev = null;
+            cacheItem.next = evalCacheHead;
+            if (evalCacheHead != null)
+                evalCacheHead.prev = cacheItem;
+            evalCacheHead = cacheItem;
+            if (evalCacheTail == null)
+            {
+                evalCacheTail = cacheItem;
+            }
+        }
+
+        if (evalCache.Count > EvalCacheMaxLength)
+        {
+            evalCache.Remove(evalCacheTail.key);
+            evalCacheTail = evalCacheTail.prev;
+            evalCacheTail.next = null;
+            cacheEvictions++;
+        }
+
+        return cacheItem.score;
+    }
+
     // Produce an array of moves sorted by estimate, probably-better moves first.
     private Move[] SortedMoves(Board board, int depth)
     {
@@ -46,7 +116,7 @@ public class MyBot : IChessBot
             {
                 Move move = moves[i];
                 board.MakeMove(move);
-                moveScores[i] = BoardScore(board);
+                moveScores[i] = CachedBoardScore(board);
                 board.UndoMove(move);
             }
             Array.Sort(moveScores, moves);
@@ -64,7 +134,7 @@ public class MyBot : IChessBot
             return 0;
 
         if (depth == 0)
-            return BoardScore(board);
+            return CachedBoardScore(board);
 
         // int bestScore = lowerBound;
         int bestScore = -MaxScore;
@@ -108,6 +178,9 @@ public class MyBot : IChessBot
     {
         int thinkingTimeGoal = ThinkingTimeGoal(timer);
         evalCount = 0;  //#DEBUG
+        cacheHits = 0;  //#DEBUG
+        cacheMisses = 0;  //#DEBUG
+        cacheEvictions = 0;  //#DEBUG
 
         // set initial search depth based on the game length
         if (searchDepth == 0)
@@ -153,6 +226,8 @@ public class MyBot : IChessBot
 
         displayer.Print();  //#DEBUG
         Console.WriteLine("evaluated positions: {0:D}k", evalCount/1000);  //#DEBUG
+        Console.WriteLine("cache hits/misses/evictions: {0:D}/{1:D}/{2:D}", cacheHits, cacheMisses, cacheEvictions);  //#DEBUG
+        Console.WriteLine("cache size: {0:D}", evalCache.Count);  //#DEBUG
         Console.WriteLine("thinking time: {0:D}/{1:D}ms", timer.MillisecondsElapsedThisTurn, thinkingTimeGoal);  //#DEBUG
         if (!wasInCheck)  // don't update the search depth in special cases
         {
